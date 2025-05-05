4a91c81ee4c0c28435e05f8b37a4c83199b037eb
PasandiLuka <lukapasandi@gmail.com>
2025-05-05 11:07:34 -0300

    Archivo de validaciones

--- Proyecto/Validaciones.cs
+++ Proyecto/Validaciones.cs
@@ -2,42 +2,52 @@ namespace Proyecto
 {
     public class Validaciones
     {
-        public static void CadenaMin (string nombreAtributo, int minimo, string mensaje)
-    {
-        if((nombreAtributo.Length <=minimo) || nombreAtributo.Contains(" "))
+        public static void CadenaEspMin (string nombreAtributo, int minimo, string mensaje)
         {
-            new ArgumentException(mensaje);
+            if((nombreAtributo.Length <=minimo) || nombreAtributo.Contains(" "))
+            {
+                new ArgumentException(mensaje);
+            }
         }
-    }
-    public static void Entero (int nombreAtributo)
-    {
-        if(nombreAtributo < 0)
+        public static void CadenaMin(string nombreAtributo, int minimo, string mensaje)
         {
-            new ArgumentException("El numero debe ser positivo");
+            if((nombreAtributo.Length <=minimo))
+            {
+                new ArgumentException(mensaje);
+            }
         }
-    }
-    public static void EnteroBetween(int nombreAtributo, int minimo, int maximo, string mensaje)
-    {
-        if(!(nombreAtributo >=minimo && nombreAtributo <=maximo))
+        public static void Entero (int nombreAtributo)
         {
-            throw new ArgumentException(mensaje);
+            if(nombreAtributo < 0)
+            {
+                new ArgumentException("El numero debe ser positivo");
+            }
         }
-    }//"El formato de las fechas es: 'YYYY-MM-DD'"
-    /* public static void Date(string nombreAtributo, string mensaje)
-    {
-        List<string> numeros = new List<string>{};
-        for(int i = 0; i <= 9; i++)
+        public static void EnteroBetween(int nombreAtributo, int minimo, int maximo, string mensaje)
         {
-            numeros.Add(Convert.ToString(i));   
+            if(!(nombreAtributo >=minimo && nombreAtributo <=maximo))
+            {
+                throw new ArgumentException(mensaje);
+            }
         }
-        
-        if(nombreAtributo.Length == 10 && nombreAtributo.Contains("-"))
+        public static void Date(string nombreAtributo, string mensaje)
         {
-            
-        }else
-        {
-            throw new ArgumentException();
+            if(!DateOnly.TryParse(nombreAtributo, out DateOnly resultado))
+            {
+                throw new ArgumentException(mensaje);
+            }
         }
-    } */
+        public static void CorreoElectronico(string nombreAtributo)
+        {
+            string mensaje = "El mail debe poseer el siguiente formato: minimo tres caracteres antes de '@', minimo tres caracteres entre el '@' y el punto, y tres caracteres a continuacion del '.' por ejemplo: 'hola@gmail.com";
+            if(nombreAtributo.LastIndexOf("@") < nombreAtributo.LastIndexOf("."))
+            {
+                if (!((nombreAtributo.LastIndexOf("@") >= 3) && ((nombreAtributo.Substring(nombreAtributo.LastIndexOf("@"), nombreAtributo.LastIndexOf(".") - nombreAtributo.LastIndexOf("@"))).Length >= 4) && ((nombreAtributo.Substring(nombreAtributo.LastIndexOf("."))).Length == 4)))
+                {
+                    throw new ArgumentException(mensaje); 
+                }
+            }else
+                throw new ArgumentException(mensaje);   
+        }
     }
 }
