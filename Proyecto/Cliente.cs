31f765d7a92cc710c806b876742c01f49e8814b3
PasandiLuka <lukapasandi@gmail.com>
2025-05-05 11:22:47 -0300

    Creacion cliente (falta terminar)

--- Proyecto/Cliente.cs
+++ Proyecto/Cliente.cs
@@ -2,5 +2,21 @@ namespace Proyecto;
 
 public class Cliente
 {
-    
-}
+    private int DNI {get; set;}
+    private string nombreCom {get; set;}
+    private string correoElectronico {get; set;}
+    private string telefono {get; set;}
+    private string nacionalidad {get; set;}
+    private string nacimiento {get; set;}
+
+    public Cliente(int DNI, string nombreCom, string correoElectronico, string telefono, string nacionalidad, string nacimiento)
+    {
+        Validaciones.EnteroBetween(DNI, 0, 99999999, "El DNI se debe encontrar entre 0 y 99999999");
+        this.DNI = DNI;
+        Validaciones.CadenaMin(nombreCom, 4, "El nombre completo debe poseer minimo 4 letras.");
+        this.nombreCom = nombreCom;
+        Validaciones.CorreoElectronico(correoElectronico);
+        this.correoElectronico = correoElectronico;
+        Validaciones.
+    }    
+}   
